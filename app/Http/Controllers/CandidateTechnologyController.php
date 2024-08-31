57ae16a4e18dfa9d3c11866a9d8347dd74fe8079
muhammedElnashar <mohamedelnashar01@gmail.com>
2024-08-31 20:30:58 +0300

    Structure Project

---
+++ app/Http/Controllers/CandidateTechnologyController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCandidateTechnologyRequest;
+use App\Http\Requests\UpdateCandidateTechnologyRequest;
+use App\Models\CandidateTechnology;
+
+class CandidateTechnologyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCandidateTechnologyRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CandidateTechnology $candidateTechnology)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CandidateTechnology $candidateTechnology)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCandidateTechnologyRequest $request, CandidateTechnology $candidateTechnology)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CandidateTechnology $candidateTechnology)
+    {
+        //
+    }
+}
