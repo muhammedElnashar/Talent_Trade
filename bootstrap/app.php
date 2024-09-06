f9048a65197335f371cbc0c486bea67211034ec5
muhammedElnashar <mohamedelnashar01@gmail.com>
2024-09-06 22:59:13 +0300

    Dashboard MiddleWare

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'is_admin' => \App\Http\Middleware\IsAdminMiddleware::class,
+            'is_employee' => \App\Http\Middleware\IsEmployeeMiddleware::class,
+            'is_candidate' => \App\Http\Middleware\IsCandidateMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,21 @@ if (!Auth::check()){
     ;})->name("dashboard");
 Route::get('/test', function () {return view('test');});
 Auth::routes();
-Route::get('/adminDashboard', function () { return view('dashboard.admin') ;})->name("adminDashboard");
+Route::prefix('admin')->middleware(['auth','is_admin'])->group(function () {
+    Route::get('/Dashboard', function () { return view('dashboard.admin') ;})->name("adminDashboard");
+});
+Route::prefix('employee')->middleware(['auth','is_employee'])->group(function () {
+    Route::get('/Dashboard', function () {return view('dashboard.employee');})->name("employeeDashboard");
+
+});
+
+Route::prefix('candidate')->middleware(['auth',"is_candidate"])->group(function () {
+    Route::get('/Dashboard', function () {return view('dashboard.candidate');})->name("candidateDashboard");
+    });
+
+
+Route::resource('candidate', CandidateController::class);
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/dashboard-role', function () {return view('auth/dashboard-role');})->name('dashboard_role');
 Route::resource('employee', EmployeeController::class);
 Route::resource('candidate', CandidateController::class);
-Route::get('/employeeDashboard', function () {return view('dashboard.employee');})->name("employeeDashboard");
-Route::get('/candidateDashboard', function () {return view('dashboard.candidate');})->name("candidateDashboard");
